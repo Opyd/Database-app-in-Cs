f3d9d741fbe3f07f00b8a38b9e6f46bd469a45c9
Daniel Opyd <danielopyd99@gmail.com>
2020-11-04 14:55:48 +0100

    bugfixes

--- Database/Form1.cs
+++ Database/Form1.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
-
-
 
 namespace Database {
 
@@ -20,8 +12,10 @@ namespace Database {
         public Form1() {
             InitializeComponent();
         }
+
         public int id = 1;
-        Stopwatch sw = new Stopwatch();
+        private Stopwatch sw = new Stopwatch();
+
         public static bool isValid(string text, int mode) {
             string strRegex;
             Regex re;
@@ -29,25 +23,30 @@ namespace Database {
                 case 1: // rok
                     strRegex = @"(^(1|2)[0-9]{3}$)";
                     break;
+
                 case 2: //tytul, wykonawca
                     strRegex = @"(^\w+$)";
                     break;
+
                 case 3: //pochodzenie
                     strRegex = @"(^[A-Z][a-z]*$)";
                     break;
+
                 case 4: //cena
                     strRegex = @"(^\d+,?\d*$)";
                     break;
+
                 case 5: //ocena
                     strRegex = @"(^[0-9][,]?[0-9]*$)";
                     break;
+
                 case 6: //id
                     strRegex = @"(^[0-9]+$)";
                     break;
+
                 default:
                     strRegex = @"(*)";
                     break;
-
             }
             re = new Regex(strRegex);
             if (re.IsMatch(text)) {
@@ -57,8 +56,6 @@ namespace Database {
             }
             return false;
         }
-
-
 
         private void button1_Click(object sender, EventArgs e) {
             int flaga = 0;
@@ -102,8 +99,6 @@ namespace Database {
                 dataGridView1.Rows.Add(id, i_rokwydania.Text, i_tytulalbumu.Text, i_wykonawca.Text, i_pochodzenie.Text, i_cena.Text, i_ocena.Text, i_nastanie.Checked);
                 id++;
             }
-
-
         }
 
         private void button2_Click(object sender, EventArgs e) {
@@ -116,22 +111,23 @@ namespace Database {
                 label14.Visible = false;
             }
             if (flaga == 0) {
-                foreach(DataGridViewRow row in dataGridView1.Rows) {
-                    if(row.Cells[0].Value.ToString() == i_id.Text) {
+                foreach (DataGridViewRow row in dataGridView1.Rows) {
+                    if (row.Cells[0].Value.ToString() == i_id.Text) {
                         dataGridView1.Rows.RemoveAt(row.Index);
+                        break;
                     }
                 }
             }
         }
-        private void button4_Click(object sender, EventArgs e) {
 
+        private void button4_Click(object sender, EventArgs e) {
             using (TextWriter tw = new StreamWriter("baza.txt")) {
                 for (int i = 0; i < dataGridView1.Rows.Count; i++) {
                     for (int j = 0; j < dataGridView1.Columns.Count; j++) {
                         tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
 
                         if (!(j == dataGridView1.Columns.Count - 1)) {
-                            tw.Write(",");
+                            tw.Write("_");
                         }
                     }
                     tw.WriteLine();
@@ -139,19 +135,19 @@ namespace Database {
                 MessageBox.Show("Pomyślnie wyeksportowano bazę");
             }
         }
-        int flaga_importu = 0;
+
+        private int flaga_importu = 0;
+
         private void button3_Click(object sender, EventArgs e) {
-            if(flaga_importu == 0) {
+            if (flaga_importu == 0) {
                 var lines = File.ReadAllLines("baza.txt");
                 if (lines.Count() > 0) {
                     foreach (var cellValues in lines.Skip(0)) {
-                        var cellArray = cellValues
-                            .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        var cellArray = cellValues.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
                         if (cellArray.Length == dataGridView1.Columns.Count) {
                             dataGridView1.Rows.Add(cellArray);
                             id++;
                         }
-                            
                     }
                 }
                 MessageBox.Show("Pomyślnie zaimportowano bazę");
@@ -159,7 +155,6 @@ namespace Database {
             } else {
                 MessageBox.Show("Baza została już wczytana");
             }
-            
         }
 
         private void button5_Click(object sender, EventArgs e) {
@@ -178,7 +173,7 @@ namespace Database {
                 sw.Start();
                 for (int i = 0; i < n; i++) {
                     x = rnd.Next(0, 2);
-                    if(x == 0) {
+                    if (x == 0) {
                         nastanie = false;
                     } else {
                         nastanie = true;
@@ -189,22 +184,20 @@ namespace Database {
                 }
                 sw.Stop();
                 MessageBox.Show("Czas losowania: " + sw.ElapsedMilliseconds.ToString() + " ms");
-
             }
         }
 
-
         public static string[] losowanie(int currentid) {
             Random rnd = new Random(Guid.NewGuid().GetHashCode());
-            string[] wykonawcy = new string[] { "Abba", "Metallica", "Ich troje", "Kappa Band", "Chillwagon", "Karolak" };
-            string[] tytuly = new string[] { "Woods", "Space Odyssey", "Wooden axe", "Dworfs", "Mullermilch good", "GMD" };
+            string[] wykonawcy = new string[] { "Abba", "Metallica", "Ich troje", "Band", "BBH", "Ye" };
+            string[] tytuly = new string[] { "YTZ", "XYZ Odyssey", "KKKX", "WKXA", "PXA", "GMD" };
             string[] pochodzenie = new string[] { "Polska", "Niemcy", "Stany Zjednoczone", "Kostaryka", "Watykan", "Islandia" };
             int wIndex = rnd.Next(wykonawcy.Length);
             int tIndex = rnd.Next(tytuly.Length);
             int pIndex = rnd.Next(pochodzenie.Length);
             int rok = rnd.Next(1900, 2020);
             int cena = rnd.Next(20, 200);
-            double ocena = Math.Round(rnd.NextDouble() * 10,1);
+            double ocena = Math.Round(rnd.NextDouble() * 10, 1);
             int id = currentid;
             string[] row = new string[] { id.ToString(), rok.ToString(), tytuly[tIndex], wykonawcy[wIndex], pochodzenie[pIndex], cena.ToString(), ocena.ToString() };
             return row;
